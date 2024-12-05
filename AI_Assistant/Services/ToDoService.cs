5d257a4d3c53e73cc538f9b98ee44bd89e25d444
EMH479 <everettmharris410@gmail.com>
2024-12-05 13:26:16 -0500

    Added viewmodels folder and main view model class, service folder and todo service class, and models folder and todo items class

---
+++ AI_Assistant/Services/ToDoService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AI_Assistant.Models;
+
+namespace AI_Assistant.Services
+{
+    public class ToDoService
+    {
+        private readonly List<ToDoItem> _toDoItems;
+
+        public ToDoService()
+        {
+            _toDoItems = new List<ToDoItem>();
+        }
+
+        public void AddToDoItem(ToDoItem item)
+        {
+            _toDoItems.Add(item);
+        }
+
+        public void RemoveToDoItem(ToDoItem item)
+        {
+            _toDoItems.Remove(item);
+        }
+
+        public List<ToDoItem> GetToDoItems()
+        {
+            return _toDoItems.OrderByDescending(item => item.CreatedAt).ToList();
+        }
+
+        public void MarkAsCompleted(ToDoItem item)
+        {
+            var toDoItem = _toDoItems.FirstOrDefault(i => i == item);
+            if (toDoItem != null)
+            {
+                toDoItem.IsCompleted = true;
+            }
+        }
+
+        public List<ToDoItem> GetPendingItems()
+        {
+            return _toDoItems.Where(item => !item.IsCompleted).ToList();
+        }
+
+    }
+}
---
+++ AI_Assistant/ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AI_Assistant.Models;
+using AI_Assistant.Services;
+
+namespace AI_Assistant.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<ToDoItem> ToDoItems { get; set; }
+        private readonly ToDoService _toDoService;
+        public ICommand AddCommand { get; }
+        public ICommand RemoveCommand { get; }
+        public ICommand MarkAsCompletedCommand { get; }
+
+        public MainViewModel()
+        {
+            _toDoService = new ToDoService();
+            ToDoItems = new ObservableCollection<ToDoItem>(_toDoService.GetToDoItems());
+            AddCommand = new RelayCommand(AddToDoItem);
+            RemoveCommand = new RelayCommand(RemoveToDoItem);
+            MarkAsCompletedCommand = new RelayCommand(MarkAsCompleted);
+        }
+
+        private void AddToDoItem(object parameter)
+        {
+            var newItem = new ToDoItem
+            {
+                Title = "New Task",
+                Description = "Task Description",
+                CreatedAt = DateTime.Now
+            };
+            _toDoService.AddToDoItem(newItem);
+            ToDoItems.Remove(newItem);
+            OnPropertyChanged("ToDoItems");
+
+        }
+
+        private void RemoveToDoItem(object parameter) {
+    }
+
+}
