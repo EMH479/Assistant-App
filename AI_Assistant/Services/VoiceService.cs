701dbe05e1ed31aed0c6681cd7df04da21db44dc
EMH479 <everettmharris410@gmail.com>
2024-12-18 17:27:21 -0500

    Added voice and text-to-speech services

---
+++ AI_Assistant/Services/VoiceService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace AI_Assistant.Services
+{
+    public class VoiceService
+    {
+        private readonly SpeechConfig _speechConfig;
+        public VoiceService(string subscriptionKey, string region)
+        {
+            _speechConfig = SpeechConfig.FromSubscription(subscriptionKey, region);
+        }
+
+        public async Task<string> RecognizeSpeechAsync()
+        {
+            using var recognizer = new SpeechRecognizer(_speechConfig);
+            var result = await recognizer.RecognizeOnceAsync();
+            return result.Text;
+        }
+
+        public async Task SynthesizeSpeechAsync(string text)
+        {
+            using var synthesizer = new SpeechSynthesizer(_speechConfig);
+            await synthesizer.SpeakTextAsync(text);
+        }
+    }
+}
